7b20a5647791d9a9c3900099b80e1a36f40605ad
RonPhengang <ronphengang999@gmail.com>
2024-11-27 19:10:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<Html>
+    <head>
+        <title>Login_Account</title>
+        <link rel="stylesheet" href="style.css">
+        </style>
+    </head>
+    <body>
+            <div class="container">
+                <h1 class="h1">Welcome To This Web</h1>
+                <input type="email"
+                placeholder="Email">
+                <input type="password"
+                placeholder="Password">
+                <button>Login</button>
+                <a href="reset.php">For get password?</a>
+                <img  class="img-btn" src="img.webp" alt="">
+            </div>
+         
+    </body>
+</Html>
---
+++ reset.php
@@ -0,0 +1,20 @@
+<Html>
+  <head>
+      <title>Login_Account</title>
+      <link rel="stylesheet" href="style2.css">
+  </head>
+  <body>
+          <div class="container">
+              <h1 class="h1">For get password?</h1>
+              <input type="email"
+              placeholder="Email">
+              <input type="number"
+              placeholder="Phone Number">
+              <input type="password" placeholder="New password">
+              <input type="password" placeholder="Continue Password">
+              <button>Sumbit</button>
+              <a href="index.php"><button>Sign In</button></a>
+              <img  class="img-btn" src="img.webp" alt="">
+          </div>
+  </body>
+</Html>
